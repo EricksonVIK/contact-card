21553223e8f758bf6269e87a878d75fa47409d89
Erikson <chris.s.erickson@gmail.com>
2022-10-23 13:21:13 -0400

    added install-pwa and tested

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -111,5 +111,22 @@ if ("serviceWorker" in navigator) {
   // use window load event to keep page load performant
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("./service-worker.js");
-  })
-};
+  });
+}
+
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = "visible";
+  installBtn.addEventListener("click", () => {
+    console.log("Install clicked!");
+    event.prompt();
+    installBtn.setAttribute("disabled", true);
+    installBtn.textContent = "Installed!";
+  });
+});
+
+window.addEventListener("appinstalled", (event) => {
+  console.log("👍", "appinstalled", event);
+});
